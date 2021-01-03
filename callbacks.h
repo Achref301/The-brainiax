3732eb2daaafd9ec54631b50b216c47695f710ea
aymenzaabar <73235172+aymenzaabar@users.noreply.github.com>
2021-01-03 13:57:47 +0100

    cpt:crud+alarmants
    cpt:crud+alarmants

---
+++ callbacks.h
@@ -0,0 +1,78 @@
+#include <gtk/gtk.h>
+
+
+void
+on_AZbuttonmenuprincipal_clicked       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZtreeview_row_activated            (GtkTreeView     *AZtreeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonrechercher_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonactualiser_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonsupprimer_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonmodifier_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+
+void
+on_AZbuttonajouter_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonconfirmermodif_clicked      (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonmenuprincipal_clicked       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonrechercher_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonactualiser_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonsupprimer_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonmodifier_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbuttonconfirmermodif_clicked      (GtkButton       *button,
+                                        gpointer         user_data);
+
+
+void
+on_AZbuttonajouter_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbutton_stat_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AZbutton_retour_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonactu_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
